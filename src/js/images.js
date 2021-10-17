25f3521b5c65fed40acdd32ab3b930dff3ad5487
Nati Mal <natanaelmalita@gmail.com>
2021-10-17 03:32:30 +0300

    make navigation to whait until reach section

--- src/js/images.js
+++ src/js/images.js
@@ -1,6 +1,7 @@
 import { setImage } from "./utilities.js";
 import profile from "../img/profile-cut.png";
 import logo from "../img/logo-nati.svg";
+
 setImage(".avatar", profile, undefined, (image) => {
   image.draggable = false;
 });
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,21 +1,21 @@
 ///dark mode switcher
 const darkMode = document.getElementById("switch-theme");
-
+const body = document.querySelector("body");
 if (darkMode.checked) {
-  document.querySelector("body").classList.remove("theme-light");
-  document.querySelector("body").classList.add("theme-dark");
+  body.classList.remove("theme-light");
+  body.classList.add("theme-dark");
 } else {
-  document.querySelector("body").classList.remove("theme-dark");
-  document.querySelector("body").classList.add("theme-light");
+  body.classList.remove("theme-dark");
+  body.classList.add("theme-light");
 }
 
 darkMode.addEventListener("change", (e) => {
   if (darkMode.checked) {
-    document.querySelector("body").classList.remove("theme-light");
-    document.querySelector("body").classList.add("theme-dark");
+    body.classList.remove("theme-light");
+    body.classList.add("theme-dark");
   } else {
-    document.querySelector("body").classList.remove("theme-dark");
-    document.querySelector("body").classList.add("theme-light");
+    body.classList.remove("theme-dark");
+    body.classList.add("theme-light");
   }
 });
 
@@ -25,7 +25,7 @@ const mainNav = document.querySelector("#navbar-main");
 if (hamb.checked) {
   mainNav.classList.add("full-screen");
 } else {
-  mainNav.classList.remove("full-screen");
+  mainNav.classList.remove("full-sreen");
 }
 
 hamb.addEventListener("change", (e) => {
@@ -33,5 +33,42 @@ hamb.addEventListener("change", (e) => {
     mainNav.classList.add("full-screen");
   } else {
     mainNav.classList.remove("full-screen");
+  }
+});
+
+const customNavGroup = document.querySelector(".custom-nav-group");
+
+//function that return a fulfield promise when the scrolling ended
+function waitForScrollEnd() {
+  let last_changed_frame = 0;
+  let last_x = window.scrollX;
+  let last_y = window.scrollY;
+
+  return new Promise((resolve) => {
+    function tick(frames) {
+      // We requestAnimationFrame either for 500 frames or until 20 frames with
+      // no change have been observed.
+      if (frames >= 500 || frames - last_changed_frame > 20) {
+        resolve();
+      } else {
+        if (window.scrollX != last_x || window.scrollY != last_y) {
+          last_changed_frame = frames;
+          last_x = window.scrollX;
+          last_y = window.scrollY;
+        }
+        requestAnimationFrame(tick.bind(null, frames + 1));
+      }
+    }
+    tick(0);
+  });
+}
+
+customNavGroup.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav-link")) {
+    if (hamb.checked) {
+      waitForScrollEnd().then(() => {
+        hamb.click();
+      });
+    }
   }
 });
