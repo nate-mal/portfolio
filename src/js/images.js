7ef087acb0cef8065ad873c087e03ddce3059eb2
Nati Mal <natanaelmalita@gmail.com>
2021-10-19 23:49:03 +0300

    fixed logo on the right of the screen and finish about section

--- src/js/images.js
+++ src/js/images.js
@@ -1,7 +1,16 @@
-import { setImage } from "./utilities.js";
+import { setImage, setImageAll } from "./utilities.js";
 import profile from "../img/profile-cut.png";
+import profileAbout from "../img/avatar-about.png";
 import logo from "../img/logo-nati.svg";
 
 setImage(document.querySelector(".avatar"), profile, undefined, (image) => {
   image.draggable = false;
 });
+setImage(
+  document.querySelector(".avatar-about"),
+  profileAbout,
+  undefined,
+  (image) => {
+    image.draggable = false;
+  }
+);
--- src/js/sections.js
+++ src/js/sections.js
@@ -1,16 +1,48 @@
-const heroText = document.querySelector(".hero-text-group");
+const heroTexts = document.querySelectorAll(".hero-text-group");
 
-const findMe = document.querySelector(".find-me");
+heroTexts.forEach((heroText) => {
+  const findMe = heroText.querySelector(".find-me");
+  heroText.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("find-me") &&
+      !findMe.classList.contains("expand")
+    ) {
+      findMe.classList.add("expand");
+    } else if (!e.target.classList.contains("find-me")) {
+      findMe.classList.remove("expand");
+    }
+  });
+});
 
-heroText.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("find-me") &&
-    !findMe.classList.contains("expand")
-  ) {
-    findMe.classList.add("expand");
-  } else if (!e.target.classList.contains("find-me")) {
-    findMe.classList.remove("expand");
-  }
+// const logoHook = document.querySelector(".hook-logo");
+// logoHook.addEventListener("mutation", () => {
+//   if (logoHook.classList.contains("active")) {
+//     document.querySelector(".logo").style.position = "absolute";
+//   }
+// });
+
+// function callback() {
+//   document.querySelector(".logo").style.position = "absolute";
+// }
+
+// function callback(mutationsList, observer) {
+//   console.log("Mutations:", mutationsList);
+//   console.log("Observer:", observer);
+// }
+function hookLogo(mutationsList) {
+  mutationsList.forEach((mutation) => {
+    if (mutation.attributeName === "class") {
+      if (document.querySelector(".hook-logo").classList.contains("active")) {
+        document.querySelector(".logo").classList.remove("logo-fixed");
+      } else {
+        document.querySelector(".logo").classList.add("logo-fixed");
+      }
+    }
+  });
+}
+const mutationObserver = new MutationObserver(hookLogo);
+mutationObserver.observe(document.querySelector(".hook-logo"), {
+  attributes: true,
 });
 
 // function component() {
