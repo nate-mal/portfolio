0e4e460aea4ef746d3313a83650b4ccb7dcf5cab
Nati Mal <natanaelmalita@gmail.com>
2021-10-21 03:51:49 +0300

    Add capabilities list

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development", //production
+  mode: "production", //production
   entry: {
     main: path.resolve(__dirname, "src/js/app.js"),
   },
